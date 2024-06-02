e74c7363217d2b6b4bce2a558518c2be4775728f
ariefro <ariefromadhon26@gmail.com>
2024-06-02 07:24:05 +0700

    add config database

---
+++ database/postgres.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"github.com/ariefro/buycut-api/config"
+	log "github.com/sirupsen/logrus"
+)
+
+func NewConnectPostgres(config *config.Config) *gorm.DB {
+	dsn := "host=" + config.PostgresHost +
+		" user=" + config.PostgresUser +
+		" password=" + config.PostgresPassword +
+		" dbname=" + config.PostgresDatabase +
+		" port=" + config.PostgresPort +
+		" sslmode=disable Timezone=Asia/Jakarta"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:                 logger.Default.LogMode((logger.Info)),
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+	if err != nil {
+		log.Error("cannot connect to database: ", err.Error())
+		os.Exit(2)
+	}
+
+	// connection pool config
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Error("cannot maintain connection pools: ", err.Error())
+		os.Exit(2)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxIdleTime(10 * time.Minute)
+	sqlDB.SetConnMaxLifetime(60 * time.Minute)
+
+	log.Info("🚀 connected successfully to the database")
+
+	return db
+}
