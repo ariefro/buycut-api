812d43662277da269fd5b769b440884d98cb2188
ariefro <ariefromadhon26@gmail.com>
2024-06-02 13:35:23 +0700

    add company model

---
+++ internal/entity/company.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+type Company struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Name      string    `gorm:"not null;unique" json:"name"`
+	Slug      string    `gorm:"not null;unique" json:"slug"`
+	Products  []Product `gorm:"foreignKey:CompanyID" json:"products,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"-"`
+}
