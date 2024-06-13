0739c14170ab4ce7c2beacab008c50558e7b1a1c
ariefro <ariefromadhon26@gmail.com>
2024-06-13 12:36:08 +0700

    change type data of id

--- internal/entity/company.go
+++ internal/entity/company.go
@@ -3,7 +3,7 @@ package entity
 import "time"
 
 type Company struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
+	ID        uint      `gorm:"primaryKey;type:serial" json:"id"`
 	Name      string    `gorm:"not null;unique" json:"name"`
 	Slug      string    `gorm:"not null;unique" json:"slug"`
 	Products  []Product `gorm:"foreignKey:CompanyID" json:"products,omitempty"`
--- internal/entity/product.go
+++ internal/entity/product.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Product struct {
-	ID        string    `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"not null" json:"name"`
-	Slug      string    `gorm:"not null" json:"slug"`
+	ID        uint      `gorm:"primaryKey;type:serial" json:"id"`
+	Name      string    `gorm:"not null;unique" json:"name"`
+	Slug      string    `gorm:"not null;unique" json:"slug"`
 	CompanyID uint      `gorm:"not null" json:"company_id"`
 	Company   *Company  `gorm:"foreignKey:CompanyID" json:"-"`
 	CreatedAt time.Time `json:"created_at"`
