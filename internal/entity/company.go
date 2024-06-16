a5578cbec8f0caa93131ab86bf49551d97d113a0
ariefro <ariefromadhon26@gmail.com>
2024-06-16 16:30:35 +0700

    change model

--- internal/entity/company.go
+++ internal/entity/company.go
@@ -3,10 +3,12 @@ package entity
 import "time"
 
 type Company struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"not null;unique" json:"name"`
-	Slug      string    `gorm:"not null;unique" json:"slug"`
-	Products  []Product `gorm:"foreignKey:CompanyID" json:"products,omitempty"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"-"`
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	Name        string    `gorm:"not null;unique" json:"name"`
+	Slug        string    `gorm:"not null;unique" json:"slug"`
+	Description string    `gorm:"not null" json:"description"`
+	ImageURL    string    `gorm:"type:varchar(255)" json:"image_url"`
+	Products    []Product `gorm:"foreignKey:CompanyID" json:"products,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"-"`
 }
--- internal/entity/product.go
+++ internal/entity/product.go
@@ -6,6 +6,7 @@ type Product struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Name      string    `gorm:"not null;unique" json:"name"`
 	Slug      string    `gorm:"not null;unique" json:"slug"`
+	ImageURL  string    `gorm:"type:varchar(255)" json:"image_url"`
 	CompanyID uint      `gorm:"not null" json:"-"`
 	Company   *Company  `gorm:"foreignKey:CompanyID" json:"company"`
 	CreatedAt time.Time `json:"created_at"`
